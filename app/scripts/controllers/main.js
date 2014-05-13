4d341550e21c2813e4c03ed342f49c677a5c105b
Guilcher Pascal <pgu.dev@gmail.com>
2014-05-13 22:03:38 +0200

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('githubClientFirebaseApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
